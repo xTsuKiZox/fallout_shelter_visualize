ee5eb09d82f8d3b3ce8373a54fc5474533148f21
TsuKiZo <tsukizooff@outlook.fr>
2024-04-16 22:42:39 +0200

    Last added and updated files

--- main.py
+++ main.py
@@ -7,38 +7,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import json
+
+with open('./other/trad.json', 'r', encoding='utf-8') as file:
+    traductions = json.load(file)
 #endregion CONSTRUCTOR
 
 #region FUNCTIONS
-# def oneShelter():
-#     chemin_dossier = os.path.expanduser("~") + "\\AppData\\Local\\FalloutShelter"
-    
-#     fichiers = [f for f in os.listdir(chemin_dossier) if os.path.isfile(os.path.join(chemin_dossier, f))]
-#     fichiers_vault = [f for f in fichiers if f.startswith("Vault") and f.endswith(".sav")]
-
-#     if fichiers_vault:
-#         driver = webdriver.Chrome()
-#         driver.get("https://falloutsheltervisualize.tsukizo.fr/")
-#         time.sleep(5)
-#         sav_file_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'sav_file')))
-#     if sav_file_input:
-#             try:
-#                 sav_file_input.click() 
-#                 time.sleep(60)  
-#                 driver.quit()  
-#             except Exception as e:
-#                 print("Erreur lors du clic sur l'élément:", e)
-#             else:
-#                 print("L'élément 'sav_file' n'a pas été trouvé sur la page.")
-#     else:
-#         print("Aucun fichier Vault n'a été trouvé dans le dossier FalloutShelter.")
-
-
-def oneShelter():
-    # Chemin du dossier FalloutShelter
+def oneShelter(lang):
     chemin_dossier = os.path.expanduser("~") + "\\AppData\\Local\\FalloutShelter"
-
-    # Liste des fichiers .sav commençant par "Vault"
     fichiers_vault = [f for f in os.listdir(chemin_dossier) if f.startswith("Vault") and f.endswith(".sav")]
 
     if fichiers_vault:
@@ -47,23 +24,83 @@ def oneShelter():
         driver.get("https://falloutsheltervisualize.tsukizo.fr/")
         input_file = driver.find_element(By.ID, 'sav_file')
         input_file.send_keys(fichier_a_telecharger)
-        # time.sleep(5)
-        input("Appuyez sur Entrée pour quitter...")
-        # driver.quit()
+        input(traductions[lang]["remerciement"])
     else:
-        print("Aucun fichier Vault n'a été trouvé dans le dossier FalloutShelter.")
+        print(traductions[lang]["sauvegardePasTrouver"])
 
-
-def twoShelter():
-    return 0
+def twoShelter(lang):
+    chemin_dossier = os.path.expanduser("~") + "\\AppData\\Local\\FalloutShelter"
+    choix2 = input(traductions[lang]["choix2"])
+    
+    if choix2 == "1":
+        fichiers_vault = [f for f in os.listdir(chemin_dossier) if f.startswith("Vault1") and f.endswith(".sav")]
+        if fichiers_vault:
+            fichier_a_telecharger = os.path.join(chemin_dossier, fichiers_vault[0])
+            driver = webdriver.Chrome()
+            driver.get("https://falloutsheltervisualize.tsukizo.fr/")
+            input_file = driver.find_element(By.ID, 'sav_file')
+            input_file.send_keys(fichier_a_telecharger)
+            input(traductions[lang]["remerciement"])
+        else:
+            print(traductions[lang]["sauvegardePasTrouver"])
+    elif choix2 == "2":
+        fichiers_vault = [f for f in os.listdir(chemin_dossier) if f.startswith("Vault2") and f.endswith(".sav")]
+        if fichiers_vault:
+            fichier_a_telecharger = os.path.join(chemin_dossier, fichiers_vault[0])
+            driver = webdriver.Chrome()
+            driver.get("https://falloutsheltervisualize.tsukizo.fr/")
+            input_file = driver.find_element(By.ID, 'sav_file')
+            input_file.send_keys(fichier_a_telecharger)
+            input(traductions[lang]["remerciement"])
+        else:
+            print(traductions[lang]["sauvegardePasTrouver"])
+    else:
+        print(traductions[lang]["erreurTape"])
 
 def threeShelter():
-    return 0
-
+        chemin_dossier = os.path.expanduser("~") + "\\AppData\\Local\\FalloutShelter"
+        choix3 = input(traductions[lang]["choix3"])
+    
+    if choix3 == "1":
+        fichiers_vault = [f for f in os.listdir(chemin_dossier) if f.startswith("Vault1") and f.endswith(".sav")]
+        if fichiers_vault:
+            fichier_a_telecharger = os.path.join(chemin_dossier, fichiers_vault[0])
+            driver = webdriver.Chrome()
+            driver.get("https://falloutsheltervisualize.tsukizo.fr/")
+            input_file = driver.find_element(By.ID, 'sav_file')
+            input_file.send_keys(fichier_a_telecharger)
+            input(traductions[lang]["remerciement"])
+        else:
+            print(traductions[lang]["sauvegardePasTrouver"])
+    elif choix3 == "2":
+        fichiers_vault = [f for f in os.listdir(chemin_dossier) if f.startswith("Vault2") and f.endswith(".sav")]
+        if fichiers_vault:
+            fichier_a_telecharger = os.path.join(chemin_dossier, fichiers_vault[0])
+            driver = webdriver.Chrome()
+            driver.get("https://falloutsheltervisualize.tsukizo.fr/")
+            input_file = driver.find_element(By.ID, 'sav_file')
+            input_file.send_keys(fichier_a_telecharger)
+            input(traductions[lang]["remerciement"])
+        else:
+            print(traductions[lang]["sauvegardePasTrouver"])
+            
+    elif choix3 == "3":
+        fichiers_vault = [f for f in os.listdir(chemin_dossier) if f.startswith("Vault3") and f.endswith(".sav")]
+        if fichiers_vault:
+            fichier_a_telecharger = os.path.join(chemin_dossier, fichiers_vault[0])
+            driver = webdriver.Chrome()
+            driver.get("https://falloutsheltervisualize.tsukizo.fr/")
+            input_file = driver.find_element(By.ID, 'sav_file')
+            input_file.send_keys(fichier_a_telecharger)
+            input(traductions[lang]["remerciement"])
+        else:
+            print(traductions[lang]["sauvegardePasTrouver"])
+    else:
+        print(traductions[lang]["erreurTape"])
 #endregion FUNCTION
 
 #region IMAGE
-my_art = AsciiArt.from_image('./img/nukoutboy.png')
+my_art = AsciiArt.from_image('./other/nukoutboy.png')
 my_output = my_art.to_ascii(columns=50)
 print(my_output)
 #endregion IMAGE
@@ -74,18 +111,29 @@ print(my_output)
 #endregion TEXTE HOME
 
 #region ABRI
-abri = input("Combien avez vous d'abri ?")
+language = input('\n > FR \n > AN')
 
-if abri == "1":
-    oneShelter()
-elif abri == "2":
-    print("Vous avez 2 abris.")
-elif abri == "3":
-    print("Vous avez 3 abris.")
-else:
-    print("Le nombre est invalide. Il y a seulement 3 abris disponibles dans Fallout Shelter.")
+if language == 'FR' or "fr":
+    abri = input("Combien avez vous d'abri ?")
+    if abri == "1":
+        oneShelter("FR")
+    elif abri == "2":
+        twoShelter("FR")
+    elif abri == "3":
+        twoShelter("FR")
+    else:
+        print("Le nombre est invalide. Il y a seulement 3 abris disponibles dans Fallout Shelter.")
+        
+elif language == 'AN' or "an":
+    abri = input("How many shelters do you have?")
+    if abri == "1":
+        oneShelter("AN")
+    elif abri == "2":
+        twoShelter("AN")
+    elif abri == "3":
+        twoShelter("AN")
+    else:
+        print("The number is invalid. There are only 3 shelters available in Fallout Shelter.")     
+else: 
+    print("Vous n'avez pas choisi la langue en l'ecrivant, fermeture du terminal | You did not choose the language when writing it, closing the terminal")    
 #endregion ABRI
-
-#region QUITTER
-input("Appuyez sur Entrée pour quitter...")
-#endregion QUITTER
